3be493adf739f7c247ec9f44d6eadec94b0a79f6
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-13 15:36:52 +0900

    5월 13일 오후 2차
    - 애니메이션 블렌드
    - 변수를 사용한 애니메이션 전환법

--- Assets/FPSGame/Script/PlayerControl.cs
+++ Assets/FPSGame/Script/PlayerControl.cs
@@ -5,6 +5,7 @@ namespace FPSGame
     public class PlayerControl : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private Animator refAnimator;
 
         private Transform reftransform;
 
@@ -17,6 +18,9 @@ namespace FPSGame
         {
             float hori = Input.GetAxis("Horizontal");
             float verti = Input.GetAxis("Vertical");
+
+            if (hori == 0 && verti==0) { refAnimator.SetInteger("State", 0); }
+            else { refAnimator.SetInteger("State", 1); }
 
             reftransform.position += new Vector3(hori, 0f, verti).normalized * moveSpeed * Time.deltaTime;
         }
